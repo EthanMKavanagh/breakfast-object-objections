a6fc13edcbb1c359e1a9840db319ce35b10f53f6
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-08-25 20:20:46 -0500

    Completed Assignment

--- client.js
+++ client.js
@@ -8,17 +8,48 @@ let refrigeratorContents  = {
   tomatoes: 1,
 };
 
+let pickyEaters = 4;
+
 // You shouldn't need to change anything above this line initially
 // You may change it to test different scenarios, but it should work as is
 
-function breakfastChecker(contents) {
-  // Your code goes here
+function breakfastChecker( contents ){
+  if( contents.eggs >= ( pickyEaters * 2 ) ){
+    console.log( 'Enough Eggs!' );
+  } // end if: eggs
+  else{
+    console.log( 'Not Enough Eggs!' );
+  } // end else: eggs
 
+  if( contents.baconStrips >= ( pickyEaters * 3 ) ){
+    console.log( 'Enough Bacon Strips!' );
+  } // end if: baconStrips
+  else{
+    console.log( 'Not Enough Bacon Strips!' );
+  } // end else: baconStrips
 
+  if( ( contents.pancakeMixBags * 10 ) >= ( pickyEaters * 3 ) ){
+    console.log( 'Enough Pancake Mix!' );
+  } // end if: pancakeMixBags
+  else{
+    console.log( 'Not Enough Pancake Mix!' );
+  } // end else: pancakeMixBags
 
-
-
-}
+  if( contents.chocolateChips >= ( pickyEaters * 15 ) ){
+    console.log( 'Enough Chocolate Chips!' );
+  } // end if: chocolateChips
+  else{
+    console.log( 'Not Enough Chocolate Chips!' );
+  }
+  
+  if( ( contents.eggs >= ( pickyEaters * 2 ) && contents.baconStrips >= ( pickyEaters * 3 ) ) ||
+  ( ( contents.pancakeMixBags * 10 ) >= ( pickyEaters * 3 ) && contents.chocolateChips >= (pickyEaters * 15 ) ) ){
+    return 'Yes!';
+  } // end if
+  else{
+    return 'No!';
+  } // end else
+} // end breakfastChecker
 
 // Your function should return 'yes' or 'no'
-console.log('Does the refrigerator have enough food for breakfast?', breakfastChecker(refrigeratorContents));
+console.log( 'Does the refrigerator have enough food for breakfast?', breakfastChecker( refrigeratorContents ) );
